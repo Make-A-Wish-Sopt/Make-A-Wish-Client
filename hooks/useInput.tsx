49f8550e70abd82144c75513cec608379f229a2f
myeongheonhong <ruddyh7076@gmail.com>
2023-04-28 02:02:44 +0900

    [ feat ] 입력제한 조건 추가

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -1,10 +1,10 @@
 import { useState, useCallback } from 'react';
 
-export default function useInput(initValue: string) {
+export default function useInput(initValue: string, maxInput: number) {
   const [value, setValue] = useState(initValue);
 
   const changeInput = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
+    e.currentTarget.value.length <= maxInput && setValue(e.currentTarget.value);
   }, []);
   return [value, changeInput] as const;
 }
