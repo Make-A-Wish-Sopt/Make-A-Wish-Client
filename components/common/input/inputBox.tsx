d4f78603e4e9e7f0b749025a575f413b7e0574d4
myeongheonhong <ruddyh7076@gmail.com>
2023-05-01 17:11:47 +0900

    [ style ] 공통 input Box 구현

---
+++ components/common/input/inputBox.tsx
@@ -0,0 +1,27 @@
+import theme from '@/styles/theme';
+import styled from 'styled-components';
+
+interface InputBoxProps {
+  children: React.ReactNode;
+}
+
+export default function InputBox(props: InputBoxProps) {
+  const { children } = props;
+
+  return <Styled.Box>{children}</Styled.Box>;
+}
+
+const Styled = {
+  Box: styled.div`
+    width: 33.1rem;
+    height: 5rem;
+
+    display: flex;
+    align-items: center;
+
+    padding: 1.4rem 1.2rem;
+    background-color: ${theme.colors.pastel_blue};
+    border: 0.1rem solid ${theme.colors.main_blue};
+    border-radius: 1rem;
+  `,
+};
