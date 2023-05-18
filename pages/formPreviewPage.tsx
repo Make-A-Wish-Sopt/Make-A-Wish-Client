3bf6334b638a02b7d51b50bd16d437d38cbc875a
myeongheonhong <ruddyh7076@gmail.com>
2023-05-18 14:19:39 +0900

    [ feat ] preview페이지에 저장된 전역변수 값 렌더 구현

--- pages/formPreviewPage.tsx
+++ pages/formPreviewPage.tsx
@@ -6,93 +6,109 @@ import InputHeader from '@/components/common/inputHeader';
 import BackBtn from '@/components/common/backBtn';
 import InputBox from '@/components/common/input/inputBox';
 import InputLargeBox from '@/components/common/input/inputLargeBox';
-
+import { useRecoilValue } from 'recoil';
+import { WishesData } from '@/recoil/formPage/wishesData';
+import ButtonBox from '@/components/button/buttonBox';
 
 export default function FormPreviewPage() {
+  const wishesData = useRecoilValue(WishesData);
 
-    return (
-        <>
-            <InputHeader>
-                <BackBtn />
-            </InputHeader>
-            <Styled.Title>소원 링크 화면 미리보기</Styled.Title>
-            <Styled.InputTitle>2023.00.00~2023.00.00</Styled.InputTitle>
+  const postWishesData = () => {
+    //API Post
+  };
 
-            <Styled.ItemBox>
-                <Styled.InputTitle>소원링크 제목</Styled.InputTitle>
-                <Styled.PresentContainer>
-                    <PresentImageBox>
-                        <Image
+  return (
+    <>
+      <InputHeader>
+        <BackBtn />
+      </InputHeader>
+      <Styled.Title>소원 링크 화면 미리보기</Styled.Title>
+      <Styled.InputTitle>
+        {wishesData.startDate}~{wishesData.endDate}
+      </Styled.InputTitle>
+
+      <Styled.ItemBox>
+        <Styled.InputTitle>{wishesData.title}</Styled.InputTitle>
+        <Styled.PresentContainer>
+          <PresentImageBox>
+            {/* <Image
                             src=""
-                            alt="선물" />
-                    </PresentImageBox>
-                    <Styled.PresentPrice>가격: 707,480원</Styled.PresentPrice>
-                </Styled.PresentContainer>
-            </Styled.ItemBox>
+                            alt="선물" /> */}
+          </PresentImageBox>
+          <Styled.PresentPrice>가격: {wishesData.price}원</Styled.PresentPrice>
+        </Styled.PresentContainer>
+      </Styled.ItemBox>
 
-            <Styled.ItemBox>
-                <InputLargeBox>
-                    <Styled.Text>선물에 대한 힌트 자유롭게</Styled.Text>
-                </InputLargeBox>
-            </Styled.ItemBox>
+      <Styled.ItemBox>
+        <InputLargeBox>
+          <Styled.Text>{wishesData.hint1}</Styled.Text>
+        </InputLargeBox>
+      </Styled.ItemBox>
 
-            <Styled.ItemBox>
-                <Styled.InputTitle>선물의 초성</Styled.InputTitle>
-                <InputBox>
-                    <Styled.Text>ㅇㅍㅇㅊ</Styled.Text>
-                </InputBox>
-            </Styled.ItemBox>
+      <Styled.ItemBox>
+        <Styled.InputTitle>선물의 초성</Styled.InputTitle>
+        <InputBox>
+          <Styled.Text>{wishesData.hint2}</Styled.Text>
+        </InputBox>
+      </Styled.ItemBox>
 
-            <Styled.ItemBox>
-                <Styled.InputTitle>예금주명/은행/계좌번호</Styled.InputTitle>
-                <InputBox>
-                    <Styled.Text>예금주명/은행/계좌번호</Styled.Text>
-                </InputBox>
-            </Styled.ItemBox>
+      <Styled.ItemBox>
+        <Styled.InputTitle>예금주명/은행/계좌번호</Styled.InputTitle>
+        <InputBox>
+          <Styled.Text>
+            {wishesData.name}/{wishesData.bankName}/{wishesData.account}
+          </Styled.Text>
+        </InputBox>
+      </Styled.ItemBox>
 
-            <Styled.ItemBox>
-                <Styled.InputTitle>연락처</Styled.InputTitle>
-                <InputBox>
-                    <Styled.Text>00000000000</Styled.Text>
-                </InputBox>
-            </Styled.ItemBox>
+      <Styled.ItemBox>
+        <Styled.InputTitle>연락처</Styled.InputTitle>
+        <InputBox>
+          <Styled.Text>{wishesData.phone}</Styled.Text>
+        </InputBox>
+      </Styled.ItemBox>
 
-            <button>소원 링크 생성하기</button>
-
-        </>
-    );
+      <ButtonBox
+        backgroundColor={theme.colors.main_blue}
+        fontColor={theme.colors.white}
+        handleClick={postWishesData}
+      >
+        소원 링크 생성하기
+      </ButtonBox>
+    </>
+  );
 }
 
 const Styled = {
-    Title: styled.h1`
+  Title: styled.h1`
     ${theme.fonts.headline24_100};
     color: ${theme.colors.main_blue};
     margin: 2.4rem 0 2rem;
-    `,
+  `,
 
-    ItemBox: styled.div`
+  ItemBox: styled.div`
     margin: 0 0 4rem;
-    `,
+  `,
 
-    InputTitle: styled.div`
+  InputTitle: styled.div`
     ${theme.fonts.body16};
     color: ${theme.colors.main_blue};
     margin: 0 0 1rem;
-    `,
+  `,
 
-    PresentContainer: styled.div`
+  PresentContainer: styled.div`
     margin: 1rem 0 0;
-    `,
+  `,
 
-    PresentPrice: styled.div`
+  PresentPrice: styled.div`
     ${theme.fonts.button16};
     color: ${theme.colors.main_blue};
     text-align: center;
-    `,
+  `,
 
-    Text: styled.div`
+  Text: styled.div`
     ${theme.fonts.body12};
     color: ${theme.colors.dark_blue};
     text-align: left;
-    `
+  `,
 };
---
+++ recoil/formPage/wishesData.ts
@@ -0,0 +1,19 @@
+import { WishesDataType } from '@/types/wishesData';
+import { atom } from 'recoil';
+
+export const WishesData = atom<WishesDataType>({
+  key: 'WishesData',
+  default: {
+    imageUrl: '',
+    price: 0,
+    title: '',
+    hint1: '',
+    hint2: '',
+    startDate: '2023-05-18',
+    endDate: '2023-05-30',
+    name: '',
+    bankName: '',
+    account: '',
+    phone: '',
+  },
+});
