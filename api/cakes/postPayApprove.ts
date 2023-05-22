c8b0e3dd481a2be022c701957ba5a6408da46220
myeongheonhong <ruddyh7076@gmail.com>
2023-05-23 00:10:17 +0900

    [ etc ] undefined 타입 추가

--- api/cakes/postPayApprove.ts
+++ api/cakes/postPayApprove.ts
@@ -2,21 +2,26 @@ import PATH from '../common/path';
 import { CakesDataType } from '@/types/cakes/cakesDataType';
 import { client } from '../common/axios';
 
-export async function postPayApprove(pgToken: string, cakesData: CakesDataType) {
-  return await client.post(
+export async function postPayApprove(
+  pgToken: string | undefined,
+  cakesData: CakesDataType | undefined,
+) {
+  const data = await client.post(
     `${PATH.API}/${PATH.V1}/${PATH.CAKES}/${PATH.PAY}/${PATH.APPROVE}`,
     {
       pgToken: pgToken,
-      tid: cakesData.tid,
+      tid: cakesData?.tid,
       partnerOrderId: process.env.NEXT_PUBLIC_ORDER_ID,
       partnerUserId: process.env.NEXT_PUBLIC_USER_ID,
-      name: cakesData.giverName,
-      cake: cakesData.selectedCake.cakeNumber,
-      message: cakesData.message,
-      wishId: cakesData.wishId,
+      name: cakesData?.giverName,
+      cake: cakesData?.selectedCake.cakeNumber,
+      message: cakesData?.message,
+      wishId: cakesData?.wishId,
     },
     {
       headers: {},
     },
   );
+
+  return data.data.data;
 }
