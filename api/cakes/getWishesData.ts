b4b883f282be5b12df373f5cf9c52924cb1a6db5
myeongheonhong <ruddyh7076@gmail.com>
2023-05-22 17:07:41 +0900

    [ feat ] 케이크 관련 api 구현

--- api/cakes/getWishesData.ts
+++ api/cakes/getWishesData.ts
@@ -1,10 +1,8 @@
-import axios from 'axios';
 import PATH from '../common/path';
+import { client } from '../common/axios';
 
 export const getWishesData = async (wishesNumber: number) => {
-  const data = await axios.get(
-    `https://www.sunmulzu.shop/${PATH.API}/${PATH.V1}/${PATH.WISHES}/${wishesNumber}`,
-  );
+  const data = await client.get(`${PATH.API}/${PATH.V1}/${PATH.WISHES}/${wishesNumber}`);
 
   return data.data.data;
 };
