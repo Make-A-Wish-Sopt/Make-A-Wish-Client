aff528ced33ff628ef60001fca51f5f134cd3903
myeongheonhong <ruddyh7076@gmail.com>
2023-05-24 02:18:54 +0900

    [ feat ] modal 커스텀 훅 구현

---
+++ components/common/modal.tsx
@@ -0,0 +1,39 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+interface ModalProps {
+  isOpen: boolean;
+  toggle: () => void;
+}
+
+export default function Modal(props: PropsWithChildren<ModalProps>) {
+  const { isOpen, toggle, children } = props;
+  return (
+    <>
+      {isOpen && (
+        <Styled.ModalOverlay onClick={toggle}>
+          <Styled.ModalContainer onClick={(e) => e.stopPropagation()}>
+            {children}
+          </Styled.ModalContainer>
+        </Styled.ModalOverlay>
+      )}
+    </>
+  );
+}
+
+const Styled = {
+  ModalOverlay: styled.div`
+    z-index: 9999;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+
+  ModalContainer: styled.div``,
+};
