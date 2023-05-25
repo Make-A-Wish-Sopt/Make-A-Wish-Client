18ef07737217bf54e2d0ea588c9423241f5431e9
myeongheonhong <ruddyh7076@gmail.com>
2023-05-26 03:30:10 +0900

    [ fix ] 인풋창 클릭 시 줌 방지 및 메타태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -49,7 +49,18 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <script src="https://developers.kakao.com/sdk/js/kakao.js" async></script>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+          />
+          <meta
+            name="description"
+            content="마음은 가볍게, 선물은 무겁게. 부담 없는 생일 펀딩 플랫폼 Make A Wish"
+          />
+          <meta
+            name="keywords"
+            content="선물하기, 카카오선물, 펀딩, 부담없는 선물, 카카오선물하기"
+          />
         </Head>
         <body>
           <Main />
