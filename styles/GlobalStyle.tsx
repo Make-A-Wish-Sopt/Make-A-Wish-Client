99c016c476f730e49226554375ab6016dbef8e15
myeongheonhong <ruddyh7076@gmail.com>
2023-05-26 00:09:54 +0900

    [ style ] 100vh 전역 스타일 수정

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -21,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
     body {
         width: 100%;
         height: 100vh;
+        height: calc(var(--vh, 1vh) * 100);
         padding: 0;
         margin: 0;
         overflow-x: hidden;
@@ -32,12 +33,15 @@ const GlobalStyle = createGlobalStyle`
         justify-content: center;
         background-color: ${theme.colors.white};
 
+        height: -webkit-fill-available;
+
         overflow: hidden;
     }
 
     body {
         width:37.5rem;
         height:100vh;
+        min-height: 100vh;
 
         padding : 2.2rem;
         background-color: ${theme.colors.bg_yellow};
