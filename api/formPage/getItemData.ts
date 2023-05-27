3cf44ff026daff9730bd783c56935c953e3eb65e
myeongheonhong <ruddyh7076@gmail.com>
2023-05-27 22:14:12 +0900

    [ feat ] 엑세스 토큰 로컬스토리지에 저장된 값으로 변경

--- api/formPage/getItemData.ts
+++ api/formPage/getItemData.ts
@@ -4,9 +4,8 @@ import { client } from '../common/axios';
 import PATH from '../common/path';
 
 export const getItemInfo = async (link: string) => {
-  const accessToken =
-    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjgzMTgxODk1LCJleHAiOjE2ODgwMjAyOTV9.RnUKwBtESVxvKU6YeXy4ssAJlEGHnIKd2LewByQhI8cPuTAh9tJQeJiMyRLYwJHFg8YwUZ667F1QRCECf1uR6w';
-  
+  const accessToken = localStorage.getItem('accessToken');
+
   const imageTag = await client.get(
     `${PATH.API}/${PATH.V1}/${PATH.WISHES}/${PATH.PRESENT}/${PATH.INFO}?url=${link}&tag=${PARSING_TAG_KEY['29cm'].imageTag}`,
     {
