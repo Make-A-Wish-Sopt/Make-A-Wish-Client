07cf442c473dca0823cae9ad11617e9d6ce960c6
myeongheonhong <ruddyh7076@gmail.com>
2023-05-28 15:30:04 +0900

    [ feat ] 엑세스 토큰 로컬스토리지에 저장

--- hooks/useAuthKakao.ts
+++ hooks/useAuthKakao.ts
@@ -3,13 +3,12 @@ import { useCallback, useEffect, useState } from 'react';
 import { TokenResponse, UserInfo, ResponseType } from '@/types/kakaoLoginType';
 import { getTokenFromKakao, sendTokenToServer, getUserFromKakao } from '@/api/kakaoLogin/login';
 
-
 export function useAuthKaKao() {
   const [nickname, setNickname] = useState<string>('');
 
   const router = useRouter();
   const { code: authCode, error: kakaoServerError } = router.query;
-  console.log("authCode : " + authCode);
+  console.log('authCode : ' + authCode);
 
   const loginHandler = useCallback(
     async (code: string) => {
@@ -17,15 +16,16 @@ export function useAuthKaKao() {
         // 1. 인가 코드를 사용해서 엑세스 토큰 가져오기
         const tokenResponse = await getTokenFromKakao(code);
         const { access_token: accessToken } = tokenResponse;
-        console.log("accessToken: " + accessToken);
+        console.log('accessToken: ' + accessToken);
 
         // 2. 엑세스 토큰을 서버로 보냄
         const apiResponse = await sendTokenToServer(accessToken);
-        console.log("apiResponse: " + apiResponse);
+        console.log('apiResponse: ' + apiResponse);
 
         if (apiResponse.success) {
           router.push('/mainPage');
-          console.log("로그인 성공_토큰 : " + apiResponse.data.accessToken);
+          console.log('로그인 성공_토큰 : ' + apiResponse.data.accessToken);
+          localStorage.setItem('accessToken', apiResponse.data.accessToken);
 
           // 오류: Cannot read properties of undefined (reading 'setAccessToken')
           // window.Kakao.Auth.setAccessToken(apiResponse.data.accessToken);
@@ -41,14 +41,14 @@ export function useAuthKaKao() {
           setNickname(nickname);
         } else {
           router.replace('/');
-          console.log("로그인 실패2 : " + apiResponse.message);
+          console.log('로그인 실패2 : ' + apiResponse.message);
         }
       } catch (error: any) {
         router.replace('/');
-        console.log("로그인 실패3 : " + error.message);
+        console.log('로그인 실패3 : ' + error.message);
       }
     },
-    [router]
+    [router],
   );
 
   useEffect(() => {
@@ -56,7 +56,7 @@ export function useAuthKaKao() {
       loginHandler(authCode as string);
     } else if (kakaoServerError) {
       router.push('/');
-      console.log("로그인 실패4 : " + kakaoServerError);
+      console.log('로그인 실패4 : ' + kakaoServerError);
     }
   }, [loginHandler, authCode, kakaoServerError, router]);
 
