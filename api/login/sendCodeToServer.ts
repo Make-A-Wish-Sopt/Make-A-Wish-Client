a59c8c5cf41305c50ae84388fd561d94a484e49a
myeongheonhong <ruddyh7076@gmail.com>
2023-08-23 19:52:36 +0900

    [ test ] 코드변경

--- api/login/sendCodeToServer.ts
+++ api/login/sendCodeToServer.ts
@@ -1,15 +1,19 @@
 import { client } from '@/api/common/axios';
 import PATH from '../../constant/path';
 
-export const sendCodeToServer = async (authCode: string) => {
-  console.log('post', authCode);
-  const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}?code=${authCode}`;
+export const sendCodeToServer = async (code: string) => {
+  const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}`;
 
-  const data = await client.post(url, {
-    headers: {
-      'Content-Type': 'application/json',
+  const data = await client.post(
+    url,
+    {},
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: { code },
     },
-  });
+  );
 
   return data.data.data;
 };
