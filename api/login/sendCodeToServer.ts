e36bbd3efac656ec4a4c2de09c8622f16176d4c3
myeongheonhong <ruddyh7076@gmail.com>
2023-08-23 18:51:02 +0900

    [ test ] api 버그테스트

--- api/login/sendCodeToServer.ts
+++ api/login/sendCodeToServer.ts
@@ -2,11 +2,11 @@ import { client } from '@/api/common/axios';
 import PATH from '../../constant/path';
 
 export const sendCodeToServer = async (authCode: string) => {
-  const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}`;
+  const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}?code=${authCode}`;
 
-  const data = await client.post(url, null, {
-    params: {
-      code: authCode,
+  const data = await client.post(url, {
+    headers: {
+      'Content-Type': 'application/json',
     },
   });
 
