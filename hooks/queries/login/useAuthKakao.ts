a856a6ffb1049150ada02dd207abe568b57b878e
myeongheonhong <ruddyh7076@gmail.com>
2023-08-23 19:38:33 +0900

    [ test ] 콘솔 추가

--- hooks/queries/login/useAuthKakao.ts
+++ hooks/queries/login/useAuthKakao.ts
@@ -40,6 +40,7 @@ export default function useAuthKakao() {
 
   useEffect(() => {
     if (authCode) {
+      console.log(authCode);
       kakaoLoginMutate();
     }
   }, [authCode, kakaoLoginMutate]);
