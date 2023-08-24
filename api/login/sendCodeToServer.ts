79382a0fb9c8b3ba6240f8f4249a7b1a95efa58e
myeongheonhong <ruddyh7076@gmail.com>
2023-08-24 11:00:57 +0900

    [ test ] 인가 설정 추가

--- api/login/sendCodeToServer.ts
+++ api/login/sendCodeToServer.ts
@@ -4,12 +4,16 @@ import PATH from '../../constant/path';
 export const sendCodeToServer = async (code: string) => {
   const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}`;
 
-  const data = await client.post(url, null, {
-    headers: {
-      'Content-Type': 'application/json',
-      code: `${code}`,
+  const data = await client.post(
+    url,
+    {},
+    {
+      headers: {
+        Authorization: code,
+        code: `${code}`,
+      },
     },
-  });
+  );
 
   return data.data.data;
 };
