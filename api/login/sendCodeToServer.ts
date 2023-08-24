1d1d8174eb216e7850d30a742e6c4079547efa7c
myeongheonhong <ruddyh7076@gmail.com>
2023-08-24 10:22:56 +0900

    [ test ] 로그인 테스트

--- api/login/sendCodeToServer.ts
+++ api/login/sendCodeToServer.ts
@@ -4,16 +4,12 @@ import PATH from '../../constant/path';
 export const sendCodeToServer = async (code: string) => {
   const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}`;
 
-  const data = await client.post(
-    url,
-    null,
-    {
-      headers: {
-        // 'Content-Type': 'application/json',
-        code: `${code}`,
-      },
+  const data = await client.post(url, null, {
+    headers: {
+      'Content-Type': 'application/json',
+      code: `${code}`,
     },
-  );
+  });
 
   return data.data.data;
 };
--- pages/login/oauth2/code/kakao/index.tsx
+++ pages/login/oauth2/code/kakao/index.tsx
@@ -1,7 +1,6 @@
 import Redirect from '@/components/login/redirect';
 
 export default function Kakao() {
-
   return (
     <>
       <Redirect />
