08bb6e74740d8c010edee48e21d3cae77ce12214
myeongheonhong <ruddyh7076@gmail.com>
2023-08-31 00:38:19 +0900

    [ fix ] redirect_uri 쿼리문 추가

--- api/login/sendCodeToServer.ts
+++ api/login/sendCodeToServer.ts
@@ -2,7 +2,7 @@ import { client } from '@/api/common/axios';
 import PATH from '../../constant/path';
 
 export const sendCodeToServer = async (code: string) => {
-  const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}`;
+  const url = `${PATH.API}/${PATH.V1}/${PATH.AUTH}/${PATH.KAKAO}/${PATH.CALLBACK}?${PATH.REDIRECT_URI}=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
 
   const data = await client.post(
     url,
