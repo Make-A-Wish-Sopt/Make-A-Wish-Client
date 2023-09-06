95ada196e740c77188d0d39295ccb8a0e4b02da8
myeongheonhong <ruddyh7076@gmail.com>
2023-09-06 14:39:37 +0900

    [ style ] 전역스타일링 변경 및 레이아웃으로 구조 변경

--- pages/login/oauth2/code/kakao/index.tsx
+++ pages/login/oauth2/code/kakao/index.tsx
@@ -1,9 +1,10 @@
+import Layout from '@/components/common/layout';
 import Redirect from '@/components/login/redirect';
 
 export default function Kakao() {
   return (
-    <>
+    <Layout>
       <Redirect />
-    </>
+    </Layout>
   );
 }
