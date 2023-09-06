02ac0a2169682409edfd1f58d77717bbdac1bdcf
myeongheonhong <ruddyh7076@gmail.com>
2023-09-06 20:00:59 +0900

    [ fix ] 푸터 위치, 버튼 위치 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,8 @@ import LoginContainer from '@/components/login';
 
 export default function LoginPage() {
   return (
-    <>
-      <Layout footer>
-        <LoginContainer />
-      </Layout>
-    </>
+    <Layout footer>
+      <LoginContainer />
+    </Layout>
   );
 }
