88dceb6a57c07b1939bdf43c137cfdf3326ec553
myeongheonhong <ruddyh7076@gmail.com>
2023-09-12 23:43:14 +0900

    [ feat ] 로그인 유효 상태 조건 추가

--- api/common/axios.ts
+++ api/common/axios.ts
@@ -1,6 +1,38 @@
+import PATH from '@/constant/path';
 import axios from 'axios';
 
 //서버통신 함수
 export const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
+
+client.interceptors.request.use(function (config: any) {
+  const token = localStorage.getItem('accessToken');
+
+  if (!token) {
+    config.headers['accessToken'] = null;
+    return config;
+  }
+
+  if (config.headers && token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+    return config;
+  }
+  return config;
+});
+
+client.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    console.log(error);
+
+    if (error.response && error.response.status === 401) {
+      if (error.response.data.message === '유효하지 않은 토큰입니다.') {
+        alert('로그인 상태를 확인해주세요!');
+        window.location.replace('/');
+      }
+    }
+  },
+);
