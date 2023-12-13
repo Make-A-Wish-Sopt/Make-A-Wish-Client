c90c202d44c426749dad81ba067e40b76ac199bc
myeongheonhong <ruddyh7076@gmail.com>
2023-12-13 16:26:12 +0900

    [ refactor ] 리액트 훅폼 적용

--- pages/wishes/index.tsx
+++ pages/wishes/index.tsx
@@ -1,10 +1,9 @@
-
 import Layout from '@/components/layout';
 import WishesFormContainer from '@/components/wishes/wishesForm';
 
 export default function WishesFormPage() {
   return (
-    <Layout layoutKey="empty">
+    <Layout layoutKey="header">
       <WishesFormContainer />
     </Layout>
   );
