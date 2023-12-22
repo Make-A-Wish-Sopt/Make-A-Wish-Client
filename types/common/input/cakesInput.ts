fc5af3c3f3d32129f08ad550ac01efbe97061b7c
myeongheonhong <ruddyh7076@gmail.com>
2023-12-22 17:12:00 +0900

    [ chore ] 대소문자 구분 커밋

--- types/common/input/cakesInput.ts
+++
@@ -1,4 +0,0 @@
-export interface CakesDataInputType {
-  giverName: string;
-  letter: string;
-}
--- utils/common/token.ts
+++
@@ -1,6 +0,0 @@
-export const getAccessToken = (): string | null => {
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem('accessToken');
-  }
-  return null;
-};
