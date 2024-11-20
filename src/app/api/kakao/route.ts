e6913b40e9aab18faf45a1dade7023a20570c809
myeongheonhong <ruddyh7076@gmail.com>
2024-11-20 20:21:31 +0900

    [ error ] api 오류 변경

--- src/app/api/kakao/route.ts
+++ src/app/api/kakao/route.ts
@@ -1,12 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(res: NextApiResponse) {
+export async function GET(res: NextRequest) {
   const clientId = process.env.KAKAO_RESTAPI_KEY;
   const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
 
   if (!clientId || !redirectUri) {
-    return res.status(500).json({ error: 'Missing Kakao API configuration' });
+    return NextResponse.json({ error: 'Missing Kakao API configuration', status: 500 });
   }
 
   const authUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
