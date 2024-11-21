c8656a00093f9d5c6d1848db7be638bb06dc2cce
myeongheonhong <ruddyh7076@gmail.com>
2024-11-21 17:30:16 +0900

    [ style ] 메인페이지 이미지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import Header from '@/components/Common/Hedaer';
 import MainPageContainer from '@/domain/home/container';
-
 import MainLayout from '@/layouts/MainLayout';
-import { getLoginUserCookiesData } from '@/utils/common/cookies';
 
 export default async function Home() {
-  const isLoggedIn = (await getLoginUserCookiesData()) ? true : false;
-
   return (
-    <>
-      <Header />
-      <MainLayout>
-        <MainPageContainer isLoggedIn={isLoggedIn} />
-      </MainLayout>
-    </>
+    <MainLayout>
+      <MainPageContainer />
+    </MainLayout>
   );
 }
--- src/domain/home/component.tsx
+++ src/domain/home/component.tsx
@@ -8,7 +8,12 @@ export function MainPageCenteredContent() {
         조물주보다 <br />
         생일선물주
       </h1>
-      <Image src={MainCakeListImg} alt="케이크 리스트 이미지" priority></Image>
+      <Image
+        src={MainCakeListImg}
+        alt="케이크 리스트 이미지"
+        priority
+        style={{ marginTop: '2rem', padding: '0 2.2rem' }}
+      />
       <span className="text-[24px] text-main_blue mt-[31px] font-bitbit">
         현금으로 선물 받는 생일잔치
       </span>
