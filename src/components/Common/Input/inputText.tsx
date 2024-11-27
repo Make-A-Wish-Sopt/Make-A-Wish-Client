039950ec018c728dc97adb4fbe1c5f80562fe028
myeongheonhong <ruddyh7076@gmail.com>
2024-11-27 13:12:44 +0900

    [ feat ] 복사 붙여넣기 기능 추가

--- src/components/Common/Input/inputText.tsx
+++ src/components/Common/Input/inputText.tsx
@@ -6,8 +6,8 @@ interface InputTextProps extends InputHTMLAttributes<HTMLInputElement> {
   register?: UseFormRegisterReturn;
   boxStyles?: React.CSSProperties;
   inputStyles?: React.CSSProperties;
-  onClick?: () => void;
   blur?: boolean;
+  keyPrevent?: boolean;
   children?: ReactNode;
 }
 
@@ -15,23 +15,39 @@ export default function InputText({
   register,
   boxStyles,
   inputStyles,
-  onClick,
   readOnly,
   children,
   blur = false,
+  keyPrevent = false,
   ...rest
 }: InputTextProps) {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (keyPrevent && !event.ctrlKey && event.key !== 'v') {
+      // Ctrl+V (붙여넣기) 허용, 나머지 키보드 입력 차단
+      event.preventDefault();
+    }
+  };
+
+  const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
+    const pastedData = event.clipboardData.getData('text');
+    if (!pastedData) {
+      event.preventDefault(); // 빈 값 붙여넣기 방지
+    }
+  };
+
   return (
     <Box bgColor="dark_green" fontColor="white" font="galmuri" styles={boxStyles}>
       <div className="flex justify-between items-center w-full h-full " style={inputStyles}>
         <input
           className="w-full h-full font-galmuri text-[14px] plachoder-gray2"
-          {...register}
-          onClick={onClick}
+          onClick={rest.onClick}
+          onPaste={handlePaste}
+          onKeyDown={handleKeyDown}
           style={blur ? { filter: 'blur(5px)' } : {}}
           readOnly={readOnly}
+          {...register}
           {...rest}
-        ></input>
+        />
         {children}
       </div>
     </Box>
