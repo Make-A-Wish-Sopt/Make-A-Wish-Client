2e9ce6064aca73ffa96aa06bd67bbd29f1090320
myeongheonhong <ruddyh7076@gmail.com>
2025-01-26 02:29:24 +0900

    [ feat ] 메인페이지 메인사진 클릭 시 가이드 모달 추가

--- src/domain/home/container.tsx
+++ src/domain/home/container.tsx
@@ -3,16 +3,29 @@
 import Image from 'next/image';
 import Button from '@/components/Common/Button';
 import { KakaoLoginIc } from '../../../public/assets/icons';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import { MainPageCenteredContent } from './component';
 import useKakaoAuth from '@/hooks/common/useKakaoAuth';
+import useToggle from '@/hooks/common/useToggle';
+import CloseTopModal from '@/components/Common/Modal/CloseTopModal';
+import { GuideImg } from '../../../public/assets/images';
 
 export default function MainPageContainer({ children }: PropsWithChildren) {
+  const modalState = useToggle();
   return (
     <>
       {children}
-      <MainPageCenteredContent />
+      <div onClick={modalState.handleState}>
+        <MainPageCenteredContent />
+      </div>
       <KakaoLoginButton />
+      {
+        <CloseTopModal isOpen={modalState.state} handleState={modalState.handleState}>
+          <div className="flex justify-center items-center w-full h-full ">
+            <Image className="w-[85%]" src={GuideImg} alt="안내 이미지" />
+          </div>
+        </CloseTopModal>
+      }
     </>
   );
 }
