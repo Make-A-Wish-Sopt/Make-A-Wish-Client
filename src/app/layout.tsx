8445f4cb7eb0da3045ebd65dd7a4b27dbea16a5c
myeongheonhong <ruddyh7076@gmail.com>
2025-01-27 16:58:48 +0900

    [ fix ] 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,20 @@ declare global {
 export const metadata: Metadata = {
   title: '조물주보다 생일선물주',
   keywords: '생일',
+  openGraph: {
+    title: '조물주보다 생일선물주',
+    description: '생일선물 플랫폼 조물주보다 생일선물주를 소개합니다.',
+    images: [
+      {
+        url: '../../public/assets/images/mainCakeListImg.png',
+        width: 1200, // 이미지 너비
+        height: 630, // 이미지 높이
+        alt: '생일선물 플랫폼 미리보기 이미지',
+      },
+    ],
+    url: 'https://sunmulzu.com', // 페이지의 기본 URL
+    siteName: '조물주보다 생일선물주',
+  },
 };
 
 export const viewport: Viewport = {
